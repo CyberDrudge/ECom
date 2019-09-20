af5cf1006c723615b062a7adb6840d3fa8ae03d0
CyberDrudge <satyamsaxena197@gmail.com>
2019-09-20 22:20:49 +0530

    Add static files

--- ecom/settings/production.py
+++ ecom/settings/production.py
@@ -76,6 +76,7 @@ FORCE_INACTIVE_USER_ENDSESSION = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,6 +116,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 # add this
 import dj_database_url
 db_from_env = dj_database_url.config()      # PostgreSQL Database in Heroku
@@ -157,7 +159,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Simplified static file serving.
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # https://kirr.co/vklau5
