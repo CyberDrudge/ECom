df9e576d0fef54c5c8b8d9afefc480f4a3acef5c
CyberDrudge <satyamsaxena197@gmail.com>
2019-11-21 17:00:16 +0530

    Add support for product images

--- products/models.py
+++ products/models.py
@@ -2,11 +2,28 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.urls import reverse
 from django.db.models import Q
+import random
 
 from ecom.utils import unique_slug_generator
 
 
 # Create your models here.
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    new_filename = random.randint(1, 3910209312)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return "products/{new_filename}/{final_filename}".format(
+            new_filename=new_filename,
+            final_filename=final_filename
+)
+
+
 class ProductQuerySet(models.query.QuerySet):
     def featured(self):
         return self.filter(featured=True)
@@ -39,6 +56,7 @@ class ProductManager(models.Manager):
 class Product(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(blank=True, unique=True)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=7)
     featured = models.BooleanField(default=False)
