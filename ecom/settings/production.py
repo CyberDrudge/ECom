574db98ad016183d45868681d2b795223ad60f1b
CyberDrudge <satyamsaxena197@gmail.com>
2019-11-24 18:53:26 +0530

    Add stripe keys

--- ecom/settings/production.py
+++ ecom/settings/production.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -21,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
+STRIPE_API_KEY = os.environ.get('STRIPE_API_KEY')
+STRIPE_PUB_KEY = os.environ.get('STRIPE_PUB_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
