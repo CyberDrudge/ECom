cc008c903aa91e2054d05499889fda5f597a8db0
CyberDrudge <satyamsaxena197@gmail.com>
2020-04-27 20:16:10 +0530

    Update config

--- ecom/config/config.py
+++ ecom/config/config.py
@@ -9,7 +9,7 @@ environ.Env.read_env(env_file=os.path.join(BASE_DIR, 'config/config.env'))
 
 SECRET_KEY = env.str('SECRET_KEY', default="")
 
-EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD', default='')
 
 STRIPE_PUB_KEY = env.str('STRIPE_PUB_KEY', default='')
 STRIPE_API_KEY = env.str('STRIPE_API_KEY', default='')
